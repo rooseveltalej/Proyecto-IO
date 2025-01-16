2cdb73caae4b6edbef97b83ffb60a21b357e199a
EsRoHi26 <es.rojas@estudiantec.cr>
2025-01-15 18:31:06 -0600

    Test 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Card } from 'react-daisyui'
+import { GiBackpack, GiPathDistance } from "react-icons/gi"
+import { ImTree } from "react-icons/im";
+import { MdScoreboard } from "react-icons/md";
+import Mochila from './pages/Mochila.tsx'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<App />} />,
+      <Route path="/mochila" element={<Mochila />} />,
+    </>
+  ))
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <RouterProvider router={router}/>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <header>
+          <h1>Menu</h1>
+          <p>Seleccione el algoritmo que desea implementar: </p>
+        </header>
+        <div className='flex flex-wrap gap-4 p-4'>
+          <Link to="/mochila" >
+          <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
+            <Card.Body>
+              <GiBackpack size={50} className='ml-16'/>
+              <Card.Title>Problema de la Mochila</Card.Title>
+            </Card.Body>
+          </Card>
+          </Link>
+
+          <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
+            <GiPathDistance size={50} className='ml-10'/>
+            <Card.Body>
+              <Card.Title>Rutas más cortas</Card.Title>
+            </Card.Body>
+          </Card>
+
+          <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
+            <ImTree size={50} className='ml-10'/>
+            <Card.Body>
+              <Card.Title>Arboles Binarios</Card.Title>
+            </Card.Body>
+          </Card>
+
+          <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
+            <MdScoreboard size={50} className='ml-10' />
+            <Card.Body>
+              <Card.Title>Series Deportivas</Card.Title>
+            </Card.Body>
+          </Card>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
