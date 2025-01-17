65923d76f396a1ac4323595ecce1a848daf7e3d4
Sebastian Matey <casamarosebas@gmail.com>
2025-01-17 15:56:17 -0600

    update

---
+++ src/pages/ArbolesBinarios.jsx
@@ -0,0 +1,12 @@
+function ArbolesBinarios() {
+
+    return (
+        <>
+            <div className="">
+                <h1></h1>
+            </div>
+        </>
+    )
+}
+
+export default ArbolesBinarios;
