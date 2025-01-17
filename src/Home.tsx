ef46d36b5c510456ab14e6f75fe9161faca4c684
Sebastian Matey <casamarosebas@gmail.com>
2025-01-17 16:59:01 -0600

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,51 +1,51 @@
-import { Card } from 'react-daisyui'
 import { GiBackpack, GiPathDistance } from "react-icons/gi"
 import { ImTree } from "react-icons/im";
 import { MdScoreboard } from "react-icons/md";
 import { Outlet, Link } from 'react-router-dom';
+import { Tooltip} from "react-daisyui";
+import './Home.css'
 
 function Home() {
     return (
         <>
             <div>
-                <header>
-                    <h1>Menu</h1>
-                    <p>Seleccione el algoritmo que desea implementar: </p>
-                </header>
-                <div className='flex flex-wrap gap-4 p-4'>
-                    <Link to='/mochila' className='text-white'>
-                    <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400 text-white' >
-                        <Card.Body>
-                            <GiBackpack size={50} className='ml-16' />
-                            <Card.Title>Problema de la Mochila</Card.Title>
-                        </Card.Body>
-                    </Card>
-                    </Link>
+                <div className='container'>
+                    <header >
+                        <h1>Menu</h1>
+                        <p>Seleccione el algoritmo que desea implementar: </p>
+                    </header>
+                    <div className='flex flex-wrap gap-4 p-4 pl-28'>
+                        <Link to='/mochila'>
+                            <div className='grow tooltip' data-tip='Problemas donde se tiene un contenedor (mochila) con o sin restricciones y se busca maximizar la ganancia' >
+                                <GiBackpack size={50} className='icon' />
+                                <p className='hiddentext'>Problema de la Mochila</p>
+                            </div>
+                        </Link>
 
-                    <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
-                        <GiPathDistance size={50} className='ml-10' />
-                        <Card.Body>
-                            <Card.Title>Rutas más cortas</Card.Title>
-                        </Card.Body>
-                    </Card>
+                        <Link to='/rutas'>
+                            <div className='grow tooltip' data-tip='Problemas donde se busca encontrar la ruta más corta entre dos puntos' >
+                                <GiPathDistance size={50} className='icon' />
+                                <p className='hiddentext'>Rutas más cortas</p>
+                            </div>
+                        </Link>
 
-                    <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
-                        <ImTree size={50} className='ml-10' />
-                        <Card.Body>
-                            <Card.Title>Arboles Binarios</Card.Title>
-                        </Card.Body>
-                    </Card>
+                        <Link to='/arboles'>
+                            <div className='grow tooltip' data-tip='Estructuras de datos que se componen de nodos, donde cada nodo tiene un valor y dos hijos' >
+                                <ImTree size={50} className='icon' />
+                                <p className='hiddentext'>Arboles Binarios</p>
+                            </div>
+                        </Link>
 
-                    <Card side="lg" className='bg-slate-500 rounded-md hover:bg-slate-400' >
-                        <MdScoreboard size={50} className='ml-10' />
-                        <Card.Body>
-                            <Card.Title>Series Deportivas</Card.Title>
-                        </Card.Body>
-                    </Card>
+                        <Link to='/series'>
+                            <div className='grow tooltip' data-tip='Problemas donde se busca encontrar la probabilidad de que gane un equipo u otro' >
+                                <MdScoreboard size={50} className='icon' />
+                                <p className='hiddentext'>Series Deportivas</p>
+                            </div>
+                        </Link>
 
+                    </div>
                 </div>
             </div>
-            <Outlet />
         </>
     )
 }
