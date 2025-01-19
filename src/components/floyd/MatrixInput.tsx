40b893205c5397846ded43b1bd64c82b01e110bf
rooseveltalej <roperez@estudiantec.cr>
2025-01-18 23:51:54 -0600

    Enhance MatrixInput component with header for adjacency matrix input

--- src/components/floyd/MatrixInput.tsx
+++ src/components/floyd/MatrixInput.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 const MatrixInput = ({ matrix, handleInputChange }) => {
+  const getNodeName = (index) => String.fromCharCode(65 + index);
+
   return (
     <div>
       <h2>Input Matrix (Adjacency)</h2>
       <table>
+        <thead>
+          <tr>
+            <th></th> {/* Celda vacía en la esquina superior izquierda */}
+            {matrix.map((_, index) => (
+              <th key={index}>{getNodeName(index)}</th>
+            ))}
+          </tr>
+        </thead>
         <tbody>
           {matrix.map((row, i) => (
             <tr key={i}>
+              <th>{getNodeName(i)}</th>
               {row.map((value, j) => (
                 <td key={j}>
                   <input
